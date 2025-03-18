21db231b6ad0c65b9dca79fa122a21381080972c
DrBryanMan <120161900+DrBryanMan@users.noreply.github.com>
2025-03-18 15:55:30 +0200

    Update loadData.js

--- js/loadData.js
+++ js/loadData.js
@@ -18,14 +18,14 @@ export let dataVolumes, dataCharacters, dataPeople, dataPublishers
 export async function loadDBData() {
   // dataVolumes = await fetch('json/Volumes Test.json').then(res => res.json())
   [dataCharacters, dataPeople, dataPublishers, dataVolumes] = await Promise.all([
-    fetch('json/Characters Test.json').then(res => res.json()),
+    fetch('json/Characters.json').then(res => res.json()),
     // fetch('json/Concepts.json').then(res => res.json()),
     // fetch('json/Locations.json').then(res => res.json()),
     // fetch('json/Objects.json').then(res => res.json()),
     fetch('json/People.json').then(res => res.json()),
     fetch('json/Publishers.json').then(res => res.json()),
     // fetch('json/Teams.json').then(res => res.json()),
-    fetch('json/Volumes Test.json').then(res => res.json()),
+    fetch('json/Volumes.json').then(res => res.json()),
   ])
   // dataVolumes = dataVolumes.sort((a, b) => new Date(b.date_added) - new Date(a.date_added));
   // allTeams = teamData.sort((a, b) => a.name.localeCompare(b.name))
